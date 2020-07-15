fef412a178880dcaa891394cdf6e8b432fd960e2
Cory Spicer <corys4139@gmail.com>
2020-07-15 16:55:39 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
+def input_to_index(user_input) 
   index = user_input.to_i
   index -= 1
   return index
