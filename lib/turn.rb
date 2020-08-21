c3e419bccb0bde0852b0434ee6ec1f0fbd35c844
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-21 00:29:03 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(number)
+  index = number.to_i
+  return index -1
+end
+
+def valid_move?(board, index)
+  if index >= 0 && index <=8 && (board[index] != "X" && board[index] != "O")
+    return true
+  else
+    return false
+  end
+end
+
+def move (board, index, player = "X")
+  board[index] = player
+end
+
+def turn(board)
+  puts("Please enter 1-9:")
+  number = gets.strip
+  index = input_to_index(number)
+
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
