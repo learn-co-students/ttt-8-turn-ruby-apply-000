ef02df7f8d14c27dfb4f0de402d2bcb77049c3b4
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-11 18:46:00 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,8 +44,8 @@ end
 #Turn Method
 def turn(board)
   puts "Please enter 1-9:"
-  num = gets.chomp
-  index = input_to_index(num)
+  input = gets.strip
+  index = input_to_index(input)
   if valid_move?(board, index) == true
     move(board, index)
     display_board(board)
