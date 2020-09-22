7f1e014404b2849d753f0240d632f19cc0301ae9
Beksultan <beksultankaiypov@gmail.com>
2020-09-22 15:45:00 +0000

    Done.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -155,30 +155,4 @@ describe './lib/turn.rb' do
 end
 
 
-def display_board(board)
-  
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def position_taken? (board, index)
-  if board[index] == "" || board[index] == " " || board[index] == nil
-    return false
-  else
-    return true
-  end
-end
-
-
-def valid_move?(board, index)
-  if !position_taken?(board, index) && (index).between?(0,8)
-    return true
-  else 
-    return false
-  end
-end
-
 
