1378e1c3a6166e120c1dacd9ebad5992201f25a6
alpha-desk-2191 <sungyonglee414@gmail.com>
2020-09-29 22:51:09 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,57 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  
+  while valid_move?(board, index) == true
+    return move(board, index, char="X") && display_board(board)
+  end
+  
+  if valid_move?(board, index) == false 
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+  end
+  
+  if valid_move?(board, index)
+      return move(board, index, char="X") && display_board(board) 
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+ 
+def input_to_index(input)
+  return input.to_i - 1
+end
+
+def move(board, index, char="X")
+  board[index] = char
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index)
+    return false
+  elsif !(index.between?(0,8))
+    return false
+  else index.between?(0,8) && !(position_taken?(board, index))
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    return false
+  elsif board[index] == nil 
+    return false 
+  else
+    return true
+  end
+end
+
+
